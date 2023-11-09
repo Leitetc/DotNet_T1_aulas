7652ddc29acdca000b44e7c052894918e9560b7c
Leitetc <thiagoleite22@hotmail.com>
2023-11-09 11:16:59 -0300

    commit3

--- Semana1/MeuProjeto1/Program.cs
+++ Semana1/MeuProjeto1/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+//dados numéricos inteiros
+ int numeroInteiro = 42;
+ decimal numeroDecimal = 3.14m;
+ long numeroLongo = 1234567890123456;
+ float numeroFlutuante = 3.14f;
+ double numeroDouble = 3.141592653589793;
+
+//Exemplos
+ System.Console.WriteLine("Exemplo de int: " + numeroInteiro);
+ Console.WriteLine("Exemplo de Decimal: " + numeroDecimal);
+ Console.WriteLine("Exemplo de Long: " + numeroLongo);
+ Console.WriteLine("Exemplo de Float: " + numeroFlutuante);
+ Console.WriteLine("Exemplo de Double: " + numeroDouble);
