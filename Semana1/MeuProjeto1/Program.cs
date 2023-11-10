51742f6c2b6970f2963dc4ff42cb4983c527c8ae
Leitetc <thiagoleite22@hotmail.com>
2023-11-10 13:56:00 -0300

    corrigindo commit1

--- Semana1/MeuProjeto1/Program.cs
+++ Semana1/MeuProjeto1/Program.cs
@@ -1,16 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Olá, Mundo!");
 
-//dados numéricos inteiros
- int numeroInteiro = 42;
- decimal numeroDecimal = 3.14m;
- long numeroLongo = 1234567890123456;
- float numeroFlutuante = 3.14f;
- double numeroDouble = 3.141592653589793;
+#region Teste de Tipos de Dados
+   
+    int tipoInteiro;
+    double tipoDouble;
+    string tipoString;
+    long tipoLong;
 
-//Exemplos
- System.Console.WriteLine("Exemplo de int: " + numeroInteiro);
- Console.WriteLine("Exemplo de Decimal: " + numeroDecimal);
- Console.WriteLine("Exemplo de Long: " + numeroLongo);
- Console.WriteLine("Exemplo de Float: " + numeroFlutuante);
- Console.WriteLine("Exemplo de Double: " + numeroDouble);
+   // tipoInteiro = tipoLong;
+    tipoInteiro = int.MaxValue;
+    tipoLong = long.MaxValue;
+    
+    tipoString = "100";
+
+    tipoInteiro = int.Parse(tipoString);
+
+    Console.WriteLine("O máximo inteiro é: " + tipoInteiro);
+    Console.WriteLine("O máximo long é: " + tipoLong);
+
+#endregion
+
+#region Operadores
+
+    tipoDouble = tipoInteiro + tipoLong;
+    //tipoInteiro = tipoDouble + tipoLong; erro, nem long nem double podem ser convertidos a um inteiro
+    //tipoLong = tipoDouble + tipoInteiro; erro, um double n pode ser convertido num long (slide 9 aula 2)
+
+    tipoInteiro = 10 > 5 ? 1 : 0;
+
+#endregion
