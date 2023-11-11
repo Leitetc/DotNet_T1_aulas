b85f2fe924ab2344f049abfba42c3594de57273c
Leitetc <thiagoleite22@hotmail.com>
2023-11-11 00:31:37 -0300

    sbyte

--- Semana1/MeuProjeto1/Program.cs
+++ Semana1/MeuProjeto1/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿/*Questão 02. Tipos de Dados:*/
+
+
+ int numeroInteiro = 42, tipoInteiro, tipoInteiro2;
+//sbyte armazena números inteiros com sinal de 8 bits
+
+
+tipoInteiro = int.MaxValue;
+tipoInteiro2 = int.MinValue;
+sbyte ValorMi = sbyte.MinValue;
+sbyte maxValue = sbyte.MaxValue;
+//Exemplos
+
+ Console.WriteLine("Maximo de tipo int: " + tipoInteiro);
+ Console.WriteLine("Minimo de tipo int: " + tipoInteiro2);
+
+
+
+Console.WriteLine("O valor mínimo de sbyte é: " + ValorMi);
+Console.WriteLine("O valor máximo de sbyte é: " + maxValue);
+/*Questão 03. Conversão de Tipos de Dados:*/
+
+/*Questão 04. Operadores Aritméticos:*/
+
+/*Questão 05. Operadores de Comparação:*/
+
+/*Questão 06. Operadores de Igualdade:*/
+
+/*Questão 07. Operadores Lógicos:*/
+
+/*Questão 08. Desafio de Mistura de Operadores:*/
