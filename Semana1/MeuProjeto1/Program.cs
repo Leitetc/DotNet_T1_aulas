f33f2447c2662d7289fcbf6e3eac768b0707bb6b
Leitetc <thiagoleite22@hotmail.com>
2023-11-10 23:48:39 -0300

    erro corrigido

--- Semana1/MeuProjeto1/Program.cs
+++ Semana1/MeuProjeto1/Program.cs
@@ -1,18 +1,1 @@
-﻿//dados numéricos inteiros
- int numeroInteiro = 42, tipoInteiro, tipoInteiro2;
- decimal numeroDecimal = 3.14m;
- long numeroLongo = 1234567890123456;
- float numeroFlutuante = 3.14f;
- double numeroDouble = 3.141592653589793;
-
-tipoInteiro = int.MaxValue;
-tipoInteiro2 = int.MinValue;
-
-//Exemplos
- System.Console.WriteLine("Exemplo de int: " + numeroInteiro);
- Console.WriteLine("Exemplo de Decimal: " + numeroDecimal);
- Console.WriteLine("Exemplo de Long: " + numeroLongo);
- Console.WriteLine("Exemplo de Float: " + numeroFlutuante);
- Console.WriteLine("Exemplo de Double: " + numeroDouble);
- Console.WriteLine("Maximo de tipo int: " + tipoInteiro);
- Console.WriteLine("Minimo de tipo int: " + tipoInteiro2);
+﻿
